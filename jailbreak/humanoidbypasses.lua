00cfb72e0c7467aafab230738c8c73e6d6ac74f2
TealingYT <58912947+TealingYT@users.noreply.github.com>
2020-07-17 09:48:37 +0100

    Create humanoidbypasses.lua

---
+++ jailbreak/humanoidbypasses.lua
@@ -0,0 +1,5 @@
+-- Made by Tealing
+-- This allows you to change walkspeed, jumppower etc.
+-- Stops you from being able to do anything like punching jewel boxes and entering cars
+
+game:GetService("Players").LocalPlayer.Character.Humanoid.Name = "Humanoidd"
