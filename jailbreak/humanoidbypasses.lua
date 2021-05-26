4a9441641feb2dbcd1cde2b8bd79c848ab0c40ec
tealingg <tealingg@gmail.com>
2021-05-26 08:20:48 +0100

    Delete jailbreak directory

--- jailbreak/humanoidbypasses.lua
+++
@@ -1,5 +0,0 @@
--- Made by Tealing
--- This allows you to change walkspeed, jumppower etc.
--- Stops you from being able to do anything like punching jewel boxes and entering cars
-
-game:GetService("Players").LocalPlayer.Character.Humanoid.Name = "Humanoidd"
